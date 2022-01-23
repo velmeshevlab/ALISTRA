a3bce0b707cc00d44f6ad1615462ec5d40a8d396
DmitryVel <dvelmeshev@gmail.com>
2022-01-23 00:25:26 -0800

    Update multilineage_plot.R

--- R/multilineage_plot.R
+++ R/multilineage_plot.R
@@ -232,14 +232,14 @@ return(pt)
 }
 
 #' @export
-plot_multiple <- function(cds, gene, lineages, colors = c("red", "blue", "green", "cyan", "magenta", "purple", "orange", "black", "yellow", "tan"), colors.mids = c("yellow", "deepskyblue", "darkolivegreen1"), N = 500, legend_position = "right"){
+plot_multiple <- function(cds, gene, lineages, start, colors = c("red", "blue", "green", "cyan", "magenta", "purple", "orange", "black", "yellow", "tan"), colors.mids = c("yellow", "deepskyblue", "darkolivegreen1"), N = 500, legend_position = "right"){
 exp = compress_expression(cds, lineages[1], gene = gene, N)
 pt = exp[,3]
 dd = as.data.frame(pt)
 cols = c("pseudotime")
 fits = c()
 for(lineage in lineages){
-exp = compress_expression(cds, lineage, gene = gene, N)
+exp = compress_expression(cds, lineage, start, gene = gene, N)
 dd = cbind(dd, exp[1:2])
 cols = append(cols, paste0("exp_", lineage))
 cols = append(cols, paste0("fit_", lineage))

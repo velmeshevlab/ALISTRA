d87ad66efc3ae9127970afa6b7456bde951bbae4
DmitryVel <dvelmeshev@gmail.com>
2022-01-24 23:39:30 -0800

    Update lineage.R

--- R/lineage.R
+++ R/lineage.R
@@ -276,7 +276,7 @@ return(cds)
 }
 
 #' @export
-setClass("cell_data_set_ext", contains = "cell_data_set", slots=c(graphs = "list", lineages="list", expression="list", expectation="list")) -> cell_data_set_ext
+setClass("cell_data_set_ext", contains = "cell_data_set", slots=c(graphs = "list", lineages="list", expression="list", expectation="list", pseudotime="list")) -> cell_data_set_ext
 
 #' @export
 import_monocle <-function(cds){

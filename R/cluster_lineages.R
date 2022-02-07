c19fe09ed7ab9e412f5c26165ccd776592358648
DmitryVel <dvelmeshev@gmail.com>
2022-02-07 12:10:52 -0800

    Update cluster_lineages.R

--- R/cluster_lineages.R
+++ R/cluster_lineages.R
@@ -15,7 +15,7 @@ d = GetAssayData(object = data, assay = "RNA", slot = "data")
 cds_name = deparse(substitute(cds))
 input = paste0("cells = ",cds_name,"@lineages$", lineage)
 eval(parse(text=input))
-res = lapply(genes, annotate_gene_peaks_sub, d = d, cells = cells, age = age)
+res = sapply(genes, annotate_gene_peaks_sub, d = d, cells = cells, age = age)
 res
 }
 

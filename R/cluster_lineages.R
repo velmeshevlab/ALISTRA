005f43bfc339068a2e44e710663c54b9aa65bbb8
DmitryVel <dvelmeshev@gmail.com>
2022-06-27 21:18:40 -0700

    Update cluster_lineages.R

--- R/cluster_lineages.R
+++ R/cluster_lineages.R
@@ -1,5 +1,5 @@
 #' @export
-phase_sub <- function(gene, fit, age, age.comp, factor = 0.2, factor2 = 0.5, age_factor = 0.25){
+phase_sub <- function(gene, fit, age, age.comp, factor = 0.2, factor2 = 0.5, age_factor = 0.5){
   fit = fit[,gene]
   locmin = rollapply(fit, 3, function(x) which.min(x)==2)
   locmin = which(locmin == TRUE)
@@ -9,10 +9,6 @@ phase_sub <- function(gene, fit, age, age.comp, factor = 0.2, factor2 = 0.5, age
   age_num = age.comp[inc]
   target_age = min(age[age$age_num > age_num,2])
   age_range = unique(age[age$age_num == target_age,1])
-  inc_max = min(which(fit == max(fit)))
-  age_num_max = age.comp[inc_max]
-  target_age_max = min(age[age$age_num > age_num_max,2])
-  age_range_max = unique(age[age$age_num == target_age_max,1])
   if(min(which(fit == max(fit))) < min(which(fit == min(fit)))){
     mode = "descrease"
   }
@@ -21,11 +17,11 @@ phase_sub <- function(gene, fit, age, age.comp, factor = 0.2, factor2 = 0.5, age
   }
   #if there are no local minima or maxima - steady increase
   if(length(locmax) == 0 & length(locmin) == 0){
-    c("steady",age_range,age_range_max,mode)
+    c("steady",age_range,mode)
   }
   #if there are no local maxima or but there is a minimum - burst increase
   else if(length(locmin) > 0 & length(locmax) == 0){
-    c("burst",age_range,age_range_max,mode)
+    c("burst",age_range,mode)
   }
   #if there are local maxima, there are several situations
   else if(length(locmax) == 1){
@@ -38,40 +34,40 @@ phase_sub <- function(gene, fit, age, age.comp, factor = 0.2, factor2 = 0.5, age
         if((max(fit[locmin:length(fit)]) - fit[locmin])/max(fit) > factor){
           #if there are two significant bends, it is a biphasic curve
           if((fit[locmax] - min(fit)) > max(fit)*factor){
-            c("biphasic",age_range,age_range_max,mode)
+            c("biphasic",age_range,mode)
           }
           #otherwise, it's a burst curve
           else if(max(fit) == fit[length(fit)]){
-            c("burst",age_range,age_range_max,mode)
+            c("burst",age_range,mode)
           }
           else{
-            c("plateau",age_range,age_range_max,mode)
+            c("plateau",age_range,mode)
           }
         }
-        else{c("plateau",age_range,age_range,mode)}
+        else{c("plateau",age_range,mode)}
       }
       else if((fit[locmax] - min(fit[locmax:length(fit)]))/fit[locmax] > factor2){
-        c("transient",age_range,age_range_max,mode)
+        c("transient",age_range,mode)
       }
       else if(((max(fit[1:locmin]) - fit[locmin])/max(fit)) > factor & ((fit[locmax] - fit[locmin])/fit[locmax]) > factor){
-        c("biphasic",age_range,age_range_max,mode)
+        c("biphasic",age_range,mode)
       }
       else if(max(fit) == fit[length(fit)]){
-        c("burst",age_range,age_range_max,mode)
+        c("burst",age_range,mode)
       }
-      else{c("plateau",age_range,age_range_max,mode)}
+      else{c("plateau",age_range,mode)}
     }
     else{if((fit[locmax] - min(fit[locmax:length(fit)]))/fit[locmax] > factor2){
-      c("transient",age_range,age_range_max,mode)
+      c("transient",age_range,mode)
     }
-      else{c("plateau",age_range,age_range_max,mode)}
+      else{c("plateau",age_range,mode)}
     }
   }
   else if(length(locmax) > 1){
-    c("biphasic",age_range,age_range_max,mode)
+    c("biphasic",age_range,mode)
   }
   else{
-    c("other","Adult",age_range_max,mode)
+    c("other","Adult",mode)
   }
 }
       
@@ -87,7 +83,7 @@ age_range
 }
 
 #' @export
-get_max_age <- function(cds, meta, genes = NULL, lineage, start){
+get_max_age <- function(cds, meta, genes = NULL, lineage, start, age_factor = 0.5){
 if(length(genes) == 0){
 genes = as.character(rownames(read.table(paste0(lineage, "_spec.txt"), sep = "\t")))
 }
@@ -105,7 +101,7 @@ pt.comp = SlidingWindow("mean", pt, 3, step)
 age_sel = age[names(pt), 2]
 age.comp = SlidingWindow("mean", age_sel, length(age_sel)/N, step)
 #res = pbsapply(genes, get_max_age_sub, age = age, fit = fit, pt.comp = pt.comp, age.comp = age.comp)
-res = pbsapply(genes, phase_sub, fit = fit, age = age, age.comp = age.comp)
+res = pbsapply(genes, phase_sub, fit = fit, age = age, age.comp = age.comp, age_factor = age_factor)
 res
 }
 
